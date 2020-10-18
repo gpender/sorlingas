c41e4000dba9e040dfe643098b722d1684389557
gpender <guy.pender@me.com>
2020-10-18 19:29:04 +0100

    f

--- api/routes/monday-webhook.js
+++ api/routes/monday-webhook.js
@@ -1,4 +1,4 @@
-const {request, gql } = require('graphql-request');
+const {GraphQLClient, gql } = require('graphql-request');
 const Board = require('../sequelize/sequelize').Board;
 const Pulse = require('../sequelize/sequelize').Pulse;
 
@@ -20,38 +20,42 @@ const updateOrCreate = async (model,where,newItem,beforeCreate) => {
 
 async function getPulsesFromMonday(){
     const endpoint = 'https://api.monday.com/v2'
-    const graphQLClient=new GraphQLClient(endpoint,{
+    const graphQLClient = new GraphQLClient(endpoint,{
         headers:{
             authorization:'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjg4MzY2MTU0LCJ1aWQiOjExNzI4NzY1LCJpYWQiOiIyMDIwLTEwLTE4VDEyOjAxOjUwLjAwMFoiLCJwZXIiOiJtZTp3cml0ZSJ9.v5lu-jdXzllMdHdguRyTA7ibTmXfeOITs-8bwztm9wk'
         },
     })
+
+    const query2 = gql`
+        query { boards (ids: 802770868) { columns { title settings_str } } }
+    `
     const query = gql`
     query {
-        me {
-          name
-        }
-        
+       
         # boards(ids:[13542, 68097]) {
-        boards(limit:1) {
+            
+        boards(ids:[802770868, 68097],limit:1) {
+
+          columns { title settings_str }
+          id
           name
-          
-          columns {
-            title
-            id
-            type
-          }
-          
           groups {
               title
             id
           }
           
           items {
+            id
             name
+            created_at
             group {
               id
             }
             
+            updates{
+                text_body
+            }
+
             column_values {
               id
               value
@@ -61,12 +65,24 @@ async function getPulsesFromMonday(){
         }
       }
     `
-    const data = await graphQLClient.request(query);
+
+    //await graphQLClient.request(query);
+    const data = await graphQLClient.request(query);//data;
+    return data;//JSON.stringify(data, undefined,2);//data;
     console.log(JSON.stringify(data, undefined,2));
+    return data;
+    return JSON.stringify({'guy':'543534'});// 
 }
 
 
 module.exports = function(app,passport){  
+
+    app.get('/guytest', function(req, res) {
+        getPulsesFromMonday().then((data) => {
+            console.log(JSON.stringify(data, undefined,2));
+            res.status(200).json(data); 
+        });
+    });
     app.post('/monday/webhook', function(req, res, next) {
         var event = req.body.event;
         console.log(event);
