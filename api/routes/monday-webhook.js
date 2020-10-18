8d72a86001c1277ea997b3d139db6716bd11e899
gpender <guy.pender@me.com>
2020-10-18 15:54:15 +0100

    Updates

--- api/routes/monday-webhook.js
+++ api/routes/monday-webhook.js
@@ -7,7 +7,9 @@ const Pulse = require('../sequelize/sequelize').Pulse;
 
 module.exports = function(app,passport){  
     app.post('/webhook', function(req, res, next) {
-        console.log(req.body);
+        //console.log(req.body);
+        var event = req.body.event;
+        console.log(event.type)
         return res.status(200).json(req.body);
         const { email, password, firstName, lastName, parentClientId } = req.body;
         createUser({ email, password, firstName, lastName, parentClientId }).then(user =>
