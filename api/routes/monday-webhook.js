1ed1331d2bc76566d382e8ff5a955ed1f5e28654
gpender <guy.pender@me.com>
2020-10-18 16:01:53 +0100

    more

--- api/routes/monday-webhook.js
+++ api/routes/monday-webhook.js
@@ -9,7 +9,21 @@ module.exports = function(app,passport){
     app.post('/webhook', function(req, res, next) {
         //console.log(req.body);
         var event = req.body.event;
-        console.log(event.type)
+//        console.log(event.boardId)
+ //       console.log(event.pulseId)
+  //      console.log(event.pulseName)
+    //    console.log(event.type)
+        switch(event.type){
+            case 'create_pulse':
+                console.log(`create pulse ${event.pulseId} ${pulse.pulseName}`);
+                break;
+            case 'create_update':
+                console.log(`update pulse ${event.pulseId} ${pulse.pulseName}`);
+                break;
+            case 'update_column_value':
+                console.log(`update pulse ${event.pulseId} ${pulse.pulseName}`);
+                break;
+        }
         return res.status(200).json(req.body);
         const { email, password, firstName, lastName, parentClientId } = req.body;
         createUser({ email, password, firstName, lastName, parentClientId }).then(user =>
