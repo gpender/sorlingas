8b2c6baba89700007a403947c40af587f76d67bb
gpender <guy.pender@me.com>
2020-10-18 16:43:32 +0100

    f

--- api/routes/monday-webhook.js
+++ api/routes/monday-webhook.js
@@ -59,7 +59,7 @@ module.exports = function(app,passport){
             case 'create_update':
             case 'update_name':
             case 'update_column_value':
-                console.log(event.pulseName);
+                console.log(event);
                 updateOrCreate(Pulse, {pulseId:event.pulseId},event,null).then(pulse =>
                     console.log(`${pulse} updated successfully`)
                 ).catch(function(err){
