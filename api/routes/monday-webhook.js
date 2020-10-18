4d13839d570fdfeb245a4da485d6908191850618
gpender <guy.pender@me.com>
2020-10-18 15:23:40 +0100

    Changes to webhook for monday.com

--- api/routes/monday-webhook.js
+++ api/routes/monday-webhook.js
@@ -5,6 +5,7 @@
 module.exports = function(app,passport){  
     app.post('/webhook', function(req, res, next) {
         console.log(req.body);
+        return res.status(200).json(req.body);
         const { email, password, firstName, lastName, parentClientId } = req.body;
         createUser({ email, password, firstName, lastName, parentClientId }).then(user =>
             res.json({ user, msg: 'account created successfully' })
