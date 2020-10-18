9e10ecaaff24d3c8a72f1491f434ac562eb0fbf5
gpender <guy.pender@me.com>
2020-10-18 16:33:03 +0100

    rt

--- api/routes/monday-webhook.js
+++ api/routes/monday-webhook.js
@@ -2,7 +2,41 @@ const Board = require('../sequelize/sequelize').Board;
 const Pulse = require('../sequelize/sequelize').Pulse;
 
 
-
+const createPulse = async ({ pulseId,pulseName }) => {
+    return await Pulse.createPulse(
+        {pulseId,pulseName},
+        {returning: false, where: {pulseId: pulseId} }
+    )
+    .then(function(updatedPulse) {
+        //updateClient2 = Client.findById(Client_Id)
+        return updatedPulse;
+    })
+};
+const updatePulse = async ({ pulseId,pulseName }) => {
+    return await Pulse.update(
+        {pulseId,pulseName},
+        {returning: false, where: {pulseId: pulseId} }
+    )
+    .then(function(updatedPulse) {
+        //updateClient2 = Client.findById(Client_Id)
+        return updatedPulse;
+    })
+};
+const updateOrCreate = async (model,where,newItem,beforeCreate) => {
+    return await Pulse.findOne({where})
+        .then(item => {
+            if(!item){
+                Promise.resolve(beforeCreate)
+                .then(() =>
+                model.create(newItem)
+                    .then(item=>({}))
+                )
+            }
+            return model
+                .update(newItem, {where: where})
+                .then(item => ({item, created:false}))
+    })
+};
 
 
 module.exports = function(app,passport){  
@@ -15,12 +49,20 @@ module.exports = function(app,passport){
     //    console.log(event.type)
         switch(event.type){
             case 'create_pulse':
+                updateOrCreate(Pulse, {pulseId:event.pulseId},event,null).then(pulse =>
+                    console.log(`${pulse} created successfully`)
+                ).catch(function(err){
+                    return res.status(400).json({ message: err.errors[0].message})
+                })
+                //updatePulse(event.pulseId, event.pulseName);
                 console.log(`create pulse ${event.pulseId} ${event.pulseName}`);
                 break;
             case 'create_update':
+                updatePulse(event.pulseId, event.pulseName);
                 console.log(`update pulse ${event.pulseId} ${event.pulseName}`);
                 break;
             case 'update_column_value':
+                updatePulse(event.pulseId, event.pulseName);
                 console.log(`update pulse ${event.pulseId} ${event.pulseName}`);
                 break;
         }
