51abd74f743405b95d493ffdf40b39fef0eb350d
gpender <guy.pender@me.com>
2020-11-28 15:12:29 +0000

    Changed sorlingas to use SSI and moved navbar and footer to incldues folder

--- public/website/js/script.js
+++ public/website/js/script.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 10) {
-	    $(".navbar").addClass("bg-light");
+		$(".navbar").addClass("bg-light");
+		console.log('guy');
 	  }
 
 	  else{
@@ -17,3 +18,9 @@ window.addEventListener("load", function () {
   const loader = document.querySelector(".loader");
   loader.className += " hidden"; // class "loader hidden"
 });
+
+$(function() {
+	var page = location.pathname.split("/")[1];
+	var navElement = $('a[href="' + page + '"]')[0].parentNode;
+	$(navElement).addClass('active');
+  });
